ba65120900da9c5ebe89e878d9a47429be9befe0
Sebastian Böthin <sebastian@boethin.eu>
2013-05-13 12:48:47 +0200

    clean up

--- source/Boethin.Net.DnsTools.DnsClient/IDnsClient.cs
+++ source/Boethin.Net.DnsTools.DnsClient/IDnsClient.cs
@@ -20,9 +20,6 @@
  */
 
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Net;
 
 namespace Boethin.Net.DnsTools.DnsClient
--- source/Boethin.Net.DnsTools.Resolution/QueryState.cs
+++ source/Boethin.Net.DnsTools.Resolution/QueryState.cs
@@ -20,9 +20,6 @@
  */
 
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace Boethin.Net.DnsTools.Resolution
 {
